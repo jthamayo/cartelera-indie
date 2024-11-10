ee44f5e6b80e0e3e3c550bebea054ff97b707bc3
jthamayo <jtamayobalogh@gmail.com>
2024-11-10 23:14:29 +0100

    refactor and configure featured section carousel

--- home.js
+++ home.js
@@ -2,82 +2,81 @@ export function loadHomePage(games) {
   const main = document.getElementById("main-page");
   if (main) {
     //-------------------Visual-cue-active-tab---------------------------------
-
     const homePage = document.getElementById("home");
     const catalogPage = document.getElementById("catalog");
     homePage.setAttribute("class", "nav-link active");
     catalogPage.setAttribute("class", "nav-link");
 
-    //--------------------------------------------------------
+    //-------------------------load-main-content------------------------------
     main.innerHTML = `<section id="new-releases" class="pt-2 mt-5"></section>`;
     main.setAttribute("class", "mt-1");
-    const newSection = document.getElementById("new-releases");
-    newSection.innerHTML += `<div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-    <div class="carousel-item active">
-        <div class="position-relative">
-        <img src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/2198150/ss_ed5a338f98da1acd76d232e581e7ca0c2e088958.600x338.jpg?t=1727471389" class="d-block w-100 position-relative" alt="...">
-        <div class="dark-film z-1"></div>
-        </div>
-        <iframe class="video-preview px-2 position-absolute top-0 start-0" width="100%" height="100%" alt="..." src="https://www.youtube.com/embed/CdWpq2efN8Y?si=AS_UbQaxUrVW8RCp&amp;&rel=0&controls=0&start=0&autoplay=1&mute=1&loop=1&playlist=CdWpq2efN8Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <div class="carousel-caption d-none d-md-block">
-        <h5 class="display-1 text-start z-3 position-relative">TITLE</h5>
-        <p class="display-3 text-start z-3 position-relative">available on steam</p>
-        </div>
-    </div>
-    <div class="carousel-item">
-      <div class="position-relative">
-      <div class="dark-film z-1"></div>
-      <img src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1933840/ss_2ac856121113c322cee773ff539579eab970ea5e.600x338.jpg?t=1730734384" class="d-block w-100 position-relative" alt="...">
-      </div>
-      <iframe class="video-preview px-2 position-absolute top-0 start-0" width="100%" height="100%" src="https://www.youtube.com/embed/b0lwduu8WyY?si=BAnHwB52D9P74MVf&amp;controls=0&rel=0&controls=0&start=0&autoplay=1&mute=1&loop=1&playlist=b0lwduu8WyY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <div class="carousel-caption d-none d-md-block">
-      <h5 class="display-1 text-start z-3 position-relative">TITLE</h5>
-      <p class="display-3 text-start z-3 position-relative">available on steam</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <div class="position-relative">
-      <div class="dark-film z-1"></div>
-      <img src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/2420660/ss_62a1d600f868ac02023335d29a4c744f71fc4f97.1920x1080.jpg?t=1729124829" class="d-block w-100 position-relative" alt="...">
-      </div>
-      <iframe class="video-preview px-2 position-absolute top-0 start-0" width="100%" height="100%" alt="..." src="https://www.youtube.com/embed/IRG8m0jBGEA?si=AvjM1kVDdmUZ4x0c&amp;&rel=0&controls=0&start=0&autoplay=1&mute=1&loop=1&playlist=IRG8m0jBGEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <div class="carousel-caption d-none d-md-block">
-      <h5 class="display-1 text-start z-3 position-relative">TITLE</h5>
-      <p class="display-3 text-start z-3 position-relative">available on steam</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>`;
-    //}
-    //TODO: play games ONLY when featured carousel-item active
-    //TODO: create function to pick three latest games to be featured in home page as new releases
-    //TODO: link steam to explore cards button
-    //TODO: swap black linear-gardient for svg
-    //TODO: fix page home tab load issue
-    //TODO: add carousel next and previous previsualization
-
+    //const newSection = document.getElementById("new-releases");
+    loadFrontPageFeatured("new-releases", games);
     loadThematicCards(games, main);
+
+    //-------------------------configure-carousel-behavior-------------------------
+    const featuredCarousel = new bootstrap.Carousel("#featuredcarousel", {
+      interval: 10000,
+      ride: "carousel",
+      pause: "hover",
+    });
   }
 }
 
-function loadThematicCards(obj, main) {
+///////////////////////////////////////////////////////////////////FEATURED-GAMES//////////////////////////////////////////////////////////////////////////////
 
-  main.innerHTML += `<div id="thematicSection" class="container px-5"></div>`;
-  document.getElementById(`thematicSection`).innerHTML = `<div id="games-aesthetic"></div><div id="games-genre"></div>`
+function latestReleased(obj) {
+  const currentYear = new Date().getFullYear();
+  return obj.games.filter((game) => game.year.includes(currentYear));
+}
+
+function loadFrontPageFeatured(sectionID, obj) {
+  document.getElementById(sectionID).innerHTML += createFrontPageFeatured(obj);
+}
+
+function createFrontPageFeatured(obj) {
+  return `<div id="featuredcarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+  <div class="carousel-inner">`.concat(
+    wrapFeaturedInCarouselItem(latestReleased(obj)).join(" "),
+    `</div></div>`
+  );
+}
+
+function wrapFeaturedInCarouselItem(featuredArr) {
+  return featuredArr.map((game, index) => {
+    let isActive = index === 0 ? "active" : "";
+    let gameId = game.preview.split("/embed/")[1].split("?")[0];
+    const videoControls = `?rel=0&controls=0&start=41&autoplay=1&mute=1&loop=1&playlist=${gameId}`;
+    return `<div class="carousel-item ${isActive}">
+      <div class="position-relative">
+      <div class="dark-film z-2"></div>
+      <img src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/2420660/ss_62a1d600f868ac02023335d29a4c744f71fc4f97.1920x1080.jpg?t=1729124829" class="d-block w-75 position-relative" alt="${
+        game.title
+      } featured">
+      </div>
+      <iframe class="video-preview px-2 position-absolute top-0 start-0" width="100%" height="100%" alt="${
+        game.title
+      } videopreview" src="${
+      game.preview + videoControls
+    }" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+      <div class="carousel-caption h-100 d-none d-md-block">
+      <h5 class="display-1 z-3 position-absolute top-50 start-0 z-3">${game.title}</h5>
+      <p class="display-5 z-3 position-absolute bottom-50 start-0 z-3">new release</p>
+      <a href="${
+        game.steam_page
+      }" target="_blank" rel="noopener noreferrer" class="btn text-bg-dark text-capitalize fs-5 border-0 m-3 px-4 position-absolute bottom-0 end-0 z-3">now available on steam</a>
+      </div>
+    </div>`;
+  });
+}
+
+///////////////////////////////////////////////////////////////////THEMED-CARDS//////////////////////////////////////////////////////////////////////////////
+
+function loadThematicCards(obj, main) {
+  main.innerHTML += `<div id="thematicSection" class="container px-1 my-5"><h1>Discover your new favourite Game</h1></div>`;
+  document.getElementById(
+    `thematicSection`
+  ).innerHTML += `<div id="games-aesthetic"></div><div id="games-genre"></div>`;
 
   const aestheticsTheme = ["atmospheric", "pixel", "cozy", "cutouts"];
 
@@ -88,7 +87,8 @@ function loadThematicCards(obj, main) {
 }
 
 function gamesByAesthetic(themeArr, obj) {
-  createThematicSection(themeArr, document.getElementById("games-aesthetic"));
+  let aestheticSection = document.getElementById("games-aesthetic");
+  createThematicSection(themeArr, aestheticSection);
   let themedGames = new Array();
   themeArr.forEach((theme) => {
     themedGames = createGameAestheticThemeArr(theme, obj);
@@ -97,7 +97,8 @@ function gamesByAesthetic(themeArr, obj) {
 }
 
 function gamesByGenre(themeArr, obj) {
-  createThematicSection(themeArr, document.getElementById("games-aesthetic"));
+  let genreSection = document.getElementById("games-genre");
+  createThematicSection(themeArr, genreSection);
   let themedGames = new Array();
   themeArr.forEach((theme) => {
     themedGames = createGameGenreThemeArr(theme, obj);
@@ -107,10 +108,10 @@ function gamesByGenre(themeArr, obj) {
 
 function createThematicSection(themeArr, container) {
   themeArr.forEach((theme) => {
-    container.innerHTML += `<section id='${theme}-games' class='container mt-5'></section>`;
+    container.innerHTML += `<section id='${theme}-games' class='py-5 my-5 rounded-4'></section>`;
     document.getElementById(
       `${theme}-games`
-    ).innerHTML += `<h2 class="fs-1 fw-bold text-capitalize">${theme} Games</h2><h3 class="text-secondary">Explore the World of ${theme} Games</h3>`;
+    ).innerHTML += `<div class="text-center pb-3"><h2 class="fs-1 fw-bold text-capitalize">${theme} Games</h2><h3 class="text-secondary">Explore the World of ${theme} Games</h3></div>`;
   });
 }
 
@@ -141,7 +142,9 @@ function insertCardsIntoCarouselItem(thematicArr) {
   });
   //display carousel with the middle item card as the active element
   thematicCardsCarouselArr[Math.floor(thematicCardsCarouselArr.length / 2)] =
-    thematicCardsCarouselArr[Math.floor(thematicCardsCarouselArr.length / 2)].replace('class="carousel-item"', 'class="carousel-item active"');
+    thematicCardsCarouselArr[
+      Math.floor(thematicCardsCarouselArr.length / 2)
+    ].replace('class="carousel-item"', 'class="carousel-item active"');
   return thematicCardsCarouselArr;
 }
 
@@ -153,7 +156,7 @@ function generateThematicCards(thematicArr) {
       <div class="card-body">
       <h5 class="card-title text-uppercase">${game.title}</h5>
       <p class="card-title text-secondary">${game.release} . ${game.length} . developed by ${game.developer}</p>
-      <a href="${game.steam_page}" target="_blank" rel="noopener noreferrer" class="btn text-light border-0 m-3 px-4 position-absolute top-50 end-0">explore</a>
+      <a href="${game.steam_page}" target="_blank" rel="noopener noreferrer" class="btn text-light m-2 position-absolute top-0 end-0">explore</a>
       </div>
       </div>
       </div>`);
